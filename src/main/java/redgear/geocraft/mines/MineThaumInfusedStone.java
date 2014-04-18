39e3b2b297da8c5c9a447c11d9e07748844168d1
LordBlackhole <LordBlackhole@gmail.com>
2014-04-18 15:05:31 -0400

    Updated Thaumcraft Plugin.
    Syncs with Thaum API update in Core.

--- src/main/java/redgear/geocraft/mines/MineThaumInfusedStone.java
+++ src/main/java/redgear/geocraft/mines/MineThaumInfusedStone.java
@@ -2,88 +2,57 @@ package redgear.geocraft.mines;
 
 import java.util.Random;
 
-import net.minecraft.util.MathHelper;
-import net.minecraft.world.World;
 import redgear.core.util.SimpleItem;
 import redgear.core.world.WorldLocation;
 import redgear.geocraft.core.GeocraftConfig;
 import redgear.geocraft.generation.VeinHelper;
 import thaumcraft.api.aspects.Aspect;
 
-public class MineThaumInfusedStone extends Mine{
+public class MineThaumInfusedStone extends MineCylinder {
 
-	private int blockId;
-	
-	public MineThaumInfusedStone(int blockId) {
-		super("ThaumInfusedStone", 1, 8);
-		this.blockId = blockId;
+	public MineThaumInfusedStone(SimpleItem block) {
+		super("ThaumInfusedStone", 1, 8, block, GeocraftConfig.stone, 6);
+	}
+
+	private int nextMeta(Aspect tag, Random rand) {
+		int md = rand.nextInt(6) + 1;
+		if (rand.nextInt(3) == 0)
+			if (tag == null) {
+			} else if (tag == Aspect.AIR)
+				md = 1;
+			else if (tag == Aspect.FIRE)
+				md = 2;
+			else if (tag == Aspect.WATER)
+				md = 3;
+			else if (tag == Aspect.EARTH)
+				md = 4;
+			else if (tag == Aspect.ORDER)
+				md = 5;
+			else if (tag == Aspect.ENTROPY)
+				md = 6;
+
+		return md;
+	}
+
+	private Aspect biomeTag(WorldLocation start, Random rand) {
+
+		Aspect tag = null;
+		try {
+			Class<?> biomeHelper = Class.forName("thaumcraft.common.lib.world.biomes.BiomeHandler");
+
+			tag = (Aspect) biomeHelper.getMethod("getRandomBiomeTag", int.class, Random.class).invoke(null,
+					start.world.getBiomeGenForCoords(start.getX(), start.getZ()).biomeID, rand);
+		} catch (Exception e) {
+		}
+
+		return tag;
 	}
 
 	@Override
-	public void generate(World world, Random rand, int chunkX, int chunkZ) {
-		try{
-			int md = rand.nextInt(6) + 1;
-	        if (rand.nextInt(3) == 0){
-	        	Class biomeHelper = Class.forName("thaumcraft.common.lib.world.biomes.BiomeHandler");
-	        	
-	        	Aspect tag = (Aspect) biomeHelper.getMethod("getRandomBiomeTag", int.class, Random.class).invoke(null, world.getBiomeGenForCoords(chunkX * 16, chunkZ * 16).biomeID, rand);
-			
-	        	if (tag == null) {
-	                md = 1 + rand.nextInt(6);
-	              }
-	              else if (tag == Aspect.AIR) md = 1;
-	              else if (tag == Aspect.FIRE) md = 2;
-	              else if (tag == Aspect.WATER) md = 3;
-	              else if (tag == Aspect.EARTH) md = 4;
-	              else if (tag == Aspect.ORDER) md = 5;
-	              else if (tag == Aspect.ENTROPY) md = 6;
-	        }
-	        
-	        //generateMine(world, rand, chunkX, chunkZ, new SimpleItem(blockId, md));
-		}
-		catch(Exception e){
-			
-		}
-		
-	}
-	
-	private void generateMine(World world, Random rand, int chunkX, int chunkZ, SimpleItem block){
-		SimpleItem target = GeocraftConfig.stone;
-		final int desity = 8;
-		final int veinSize = 6;
-		
-		int centerX = chunkX * 16 + rand.nextInt(16); //defines the center of the vertical standing cylinder
-		int centerZ = chunkZ * 16 + rand.nextInt(16);
-		int top = Math.max(rand.nextInt(world.getActualHeight()), 16);
-		int bottom = Math.max(rand.nextInt(top), top - 16);
-		
-		if(bottom >= world.getHeightValue(chunkX * 16, chunkZ * 16))
-			return;
-		
-		float size = getMineSize();
-		
-		int maxRadius =  (int)Math.max(Math.sqrt((veinSize * (int)size * desity) / (top - bottom) / Math.PI), 4);
-		
-		int x = 0;
-		int y = 0;
-		int z = 0;
-		
-		float angle = 0;
-		float radius = 0;
-		
-		
-		
-		for(int i = 0; i <= size; i++){
-			y = 4 + rand.nextInt(top);
-			angle = rand.nextFloat() * (float)Math.PI;
-			radius = rand.nextInt(maxRadius);
-			
-			x = (int) (MathHelper.sin(angle) * radius);
-			z = (int) (MathHelper.cos(angle) * radius);
-			
-			
-			VeinHelper.generateSphere(new WorldLocation(x + centerX, y, z + centerZ, world), block, target, rand, veinSize);
-		}
+	protected void generateVein(WorldLocation start, SimpleItem block, SimpleItem target, Random rand, int size) {
+		VeinHelper.generateSphere(start, new SimpleItem(block.item, nextMeta(biomeTag(start, rand), rand)), target,
+				rand, size);
+
 	}
 
 }
--- src/main/java/redgear/geocraft/plugins/ThaumcraftPlugin.java
+++ src/main/java/redgear/geocraft/plugins/ThaumcraftPlugin.java
@@ -1,9 +1,15 @@
 package redgear.geocraft.plugins;
 
+import net.minecraft.item.Item;
 import redgear.core.mod.IPlugin;
 import redgear.core.mod.ModUtils;
+import redgear.core.mod.Mods;
+import redgear.core.util.ItemRegUtil;
+import redgear.core.util.SimpleItem;
+import redgear.geocraft.core.GeocraftConfig;
 import redgear.geocraft.generation.MineGenerator;
 import redgear.geocraft.generation.MineRegistry;
+import redgear.geocraft.mines.MineCylinder;
 import redgear.geocraft.mines.MineThaumInfusedStone;
 import cpw.mods.fml.common.LoaderState.ModState;
 
@@ -16,7 +22,7 @@ public class ThaumcraftPlugin implements IPlugin {
 
 	@Override
 	public boolean shouldRun(ModUtils mod, ModState state) {
-		return false;//Mods.Thaum.isIn() && inst.getBoolean("plugins", "Thaumcraft")
+		return Mods.Thaum.isIn() && mod.getBoolean("plugins", "Thaumcraft");
 	}
 
 	@Override
@@ -31,27 +37,62 @@ public class ThaumcraftPlugin implements IPlugin {
 
 	@Override
 	public void Init(ModUtils inst) {
-		try {
-			Class config = Class.forName("thaumcraft.common.config.Config");
 
-			if (config != null) {
-				int ID = config.getField("blockCustomOreId").getInt(null);
+		if(Mods.Thaum.isIn() && inst.getBoolean("plugins", "Thaumcraft")){
+			try{
+				Class<?> blocks = Class.forName("thaumcraft.common.config.Config");
 
-				MineRegistry reg = MineGenerator.reg;
+				if(blocks != null){
+					Item block = ItemRegUtil.findItem(Mods.Thaum, "blockCustomOre").item;
+					
+					
 
-				reg.registerMine(new MineThaumInfusedStone(ID));
-				/*
-				 * reg.registerIgnore(ID, 1);
-				 * reg.registerIgnore(ID, 2);
-				 * reg.registerIgnore(ID, 3);
-				 * reg.registerIgnore(ID, 4);
-				 * reg.registerIgnore(ID, 5);
-				 * reg.registerIgnore(ID, 6);
-				 */
+					MineRegistry reg = MineGenerator.reg;
+					SimpleItem stone = GeocraftConfig.stone;
+
+					
+					SimpleItem cinnibar = new SimpleItem(block, 0);
+					SimpleItem air = new SimpleItem(block, 1);
+					SimpleItem fire = new SimpleItem(block, 2);
+					SimpleItem water = new SimpleItem(block, 3);
+					SimpleItem earth = new SimpleItem(block, 4);
+					SimpleItem order = new SimpleItem(block, 5);
+					SimpleItem entropy = new SimpleItem(block, 6);
+					SimpleItem amber = new SimpleItem(block, 7);
+					
+					reg.registerMine(new MineThaumInfusedStone(cinnibar));
+
+
+					reg.registerTrace("CinnibarTrace", cinnibar, stone, 4);
+					reg.registerTrace("AirInfusedStoneTrace", air, stone, 4);
+					reg.registerTrace("FireInfusedStoneTrace", fire, stone, 4);
+					reg.registerTrace("WaterInfusedStoneTrace", water, stone, 4);
+					reg.registerTrace("EarthInfusedStoneTrace", earth, stone, 4);
+					reg.registerTrace("OrderInfusedStoneTrace", order, stone, 4);
+					reg.registerTrace("EntropyInfusedStoneTrace", entropy, stone, 4);
+					reg.registerTrace("AmberTrace", amber, stone, 4);
+
+
+
+					reg.registerMine(new MineCylinder("Amber", 4, 8, amber, stone, 4));
+					reg.registerMine(new MineCylinder("Cinnibar", 4, 8, cinnibar, stone, 4));
+					
+					
+				}
+				
+				
+				Class<?> config = Class.forName("thaumcraft.common.config.Config");
+
+				if(config != null){
+
+				config.getField("genCinnibar").setBoolean(null, false);
+				config.getField("genAmber").setBoolean(null, false);
+				config.getField("genInfusedStone").setBoolean(null, false);
+				}
 			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-			//inst.logDebug("Thaumcraft config reflection failed", e);
+			catch(Exception e){
+				inst.logDebug("Thaumcraft config reflection failed", e);
+			}
 		}
 
 	}
