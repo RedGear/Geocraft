97e937539eb1ecc1e1977b3602cf295ed77b7c13
LordBlackhole <LordBlackhole@gmail.com>
2014-07-19 21:56:57 -0400

    1.7.10 Update
    Just bumping versions as well as a tiny fix to transformer.

--- src/main/java/redgear/geocraft/core/MinableTransformer.java
+++ src/main/java/redgear/geocraft/core/MinableTransformer.java
@@ -24,6 +24,7 @@ import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.VarInsnNode;
 
+import redgear.geocraft.codechicken.DepLoader;
 import redgear.geocraft.generation.MineGenerator;
 import cpw.mods.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
@@ -89,7 +90,7 @@ public class MinableTransformer implements IClassTransformer, IFMLLoadingPlugin{
 
 	@Override
 	public String getSetupClass() {
-		return null;
+		return DepLoader.class.getName();
 	}
 
 	@Override
